36eaf17c655156ea000b0101e1940c3897d6d754
only4abhi <abhishekchanda.mailbox@gmail.com>
2017-01-11 21:31:14 +0530

    Add files via upload

---
+++ SubString.java
@@ -0,0 +1,13 @@
+
+public class SubString {
+
+	public static void main(String[] args) {
+		
+		String st="Welcome in Java";  
+		System.out.println(st.substring(0,10));
+		
+		System.out.println(st.substring(7));  
+
+	}
+
+}
